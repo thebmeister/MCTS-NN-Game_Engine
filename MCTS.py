5b6743dff9cc2a0da296c396338e77a6b16d99f4
thebmeister <35781415+thebmeister@users.noreply.github.com>
2018-01-28 20:41:55 -0800

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -366,45 +366,6 @@ def UCT(rootstate, itermax, verbose = False):
     # Output some information about the tree - can be omitted
     if (verbose): print(rootnode.TreeToString(0))
     else: print(rootnode.ChildrenToString())
-
-def MUCT(rootstate, itermax, verbose = False):
-    """ Conduct a modified UCT search for itermax iterations starting from rootstate.
-        Rollouts moves are chosen based on the neural network.
-        Return the best move from the rootstate.
-        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0]."""
-
-    rootnode = Node(state = rootstate)
-
-    for i in range(itermax):
-        node = rootnode
-        state = rootstate.Clone()
-
-        # Select
-        while node.untriedMoves == [] and node.childNodes != []: # node is fully expanded and non-terminal
-            node = node.UCTSelectChild()
-            state.DoMove(node.move)
-
-        # Expand
-        if node.untriedMoves != []: # if we can expand (i.e. state/node is non-terminal)
-            m = random.choice(node.untriedMoves) 
-            state.DoMove(m)
-            node = node.AddChild(m,state) # add child and descend tree
-
-        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
-        while state.GetMoves() != []: # while state is non-terminal
-        #    weights = softmax(network.evaluate(state.GetMoves()) # this is the idea, but it needs to be fixed
-            state.DoMove(random.choice(state.GetMoves(), p=weights))
-
-        # Backpropagate
-        while node != None : # backpropagate from the expanded node and work back to the root node
-            node.Update(state.GetResult(node.playerJustMoved)) # state is terminal. Update node with result from POV of node.playerJustMoved
-            node = node.parentNode
-
-    # Output some information about the tree - can be omitted
-    if (verbose): print(rootnode.TreeToString(0))
-    else: print(rootnode.ChildrenToString())
-
-    return sorted(rootnode.childNodes, key = lambda c: c.visits)[-1].move # return the move that was most visited
                 
 def UCTPlayGame():
     """ Play a sample game between two UCT players where each player gets a different number 
@@ -426,23 +387,6 @@ def UCTPlayGame():
     elif state.GetResult(state.playerJustMoved) == 0.0:
         print("Player " + str(3 - state.playerJustMoved) + " wins!")
     else: print("Nobody wins!")
-
-def MUTCPlayGame():
-    """ Play a sample game between to players using a UTC method modified by the Neural Network evaluation to select
-        rollouts that are likely instead of just random. Backpropagates to returns list of tuples, with the state.board
-    """
-    # state = OthelloState(4) # uncomment to play Othello on a square board of the given size
-    state = OXOState() # uncomment to play OXO
-    # state = NimState(15) # uncomment to play Nim with the given number of starting chips
-    while (state.GetMoves() != []):
-        m = UCT(rootstate = state, itermax = 100, verbose = False) # play with values for itermax and verbose = True
-        state.DoMove(m)
-    if state.GetResult(state.playerJustMoved) == 1.0:
-        print("Player " + str(state.playerJustMoved) + " wins!")
-    elif state.GetResult(state.playerJustMoved) == 0.0:
-        print("Player " + str(3 - state.playerJustMoved) + " wins!")
-    else: print("Nobody wins!")
-
 
 def UTCPlayVsHuman():
     """ Play a sample game between UCT player and human. Computer plays first.
@@ -466,8 +410,7 @@ def UTCPlayVsHuman():
             else:
                 m = eval(input("Where would you like to place your piece? \n0 1 2\n3 4 5\n6 7 8\n")) # Change if not playing OXO
                 while state.board[m] != 0:
-                    m = eval(input("Error: Please pick an empty location.\nThese are the remaining \
-empty locations:\n" + str(state.GetMoves()) + "\n"))
+                    m = eval(input("Error: Please pick an empty location.\nThese are the remaining empty locations:\n" + str(state.GetMoves()) + "\n"))
             state.DoMove(m)
         if state.GetResult(state.playerJustMoved) == humanplayer:
             print(repr(state) + "\nYou win!!\n")
