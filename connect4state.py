f4f9bf47be8d34e7d4da2c1db827d832c0d2c4ee
thebmeister <35781415+thebmeister@users.noreply.github.com>
2018-01-31 11:12:46 -0800

    Update connect4state.py
    update description

--- connect4state.py
+++ connect4state.py
@@ -10,13 +10,13 @@ to train a Connect Four engine.
 class Connect4State(object):
 	""" A state of the game, i.e. the game 'board' - 
 		I'm going to refer to it as the board from now on.
-		The board is arranged as follows:
-		35 36 37 38 39 40 41
-		28 29 30 31 32 33 34
-		21 22 23 24 25 26 27
-		14 15 16 17 18 19 20 
-		7  8  9  10 11 12 13
-		0  1  2  3  4  5  6
+		The board is arranged as follows with (height, column) indices:
+		(5,0)  (5,1)  (5,2)  (5,3)  (5,4)  (5,5)  (5,6)
+		(4,0)  (4,1)  (4,2)  (4,3)  (4,4)  (4,5)  (4,6)
+		(3,0)  (3,1)  (3,2)  (3,3)  (3,4)  (3,5)  (3,6)
+		(2,0)  (2,1)  (2,2)  (2,3)  (2,4)  (2,5)  (2,6)
+		(1,0)  (1,1)  (1,2)  (1,3)  (1,4)  (1,5)  (1,6)
+		(0,0)  (0,1)  (0,2)  (0,3)  (0,4)  (0,5)  (0,6)
 		where 0 = empty, 1 = player 1, 2 = player 2
 	"""
 	def __init__(self):
