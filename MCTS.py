8acc9569d4e0ef55aff0b74eb186f27b1931edb8
thebmeister <35781415+thebmeister@users.noreply.github.com>
2018-01-31 12:42:13 -0800

    Update MCTS.py
    Added rules for Connect 4 in the class Connect4State.
    Modified UTCPlayVsHuman to ask sensible questions regardless of the game
    Added GetHumanMove function for clarity

--- MCTS.py
+++ MCTS.py
@@ -167,7 +167,107 @@ class OXOState:
             s += ".XO"[self.board[i]] + " "
             if i % 3 == 2: s += "\n"
         return s
+    
+class Connect4State(object):
+	""" A state of the game, i.e. the game 'board' - 
+		I'm going to refer to it as the board from now on.
+		The board is arranged as follows with (height, column) indices:
+		(5,0)  (5,1)  (5,2)  (5,3)  (5,4)  (5,5)  (5,6)
+		(4,0)  (4,1)  (4,2)  (4,3)  (4,4)  (4,5)  (4,6)
+		(3,0)  (3,1)  (3,2)  (3,3)  (3,4)  (3,5)  (3,6)
+		(2,0)  (2,1)  (2,2)  (2,3)  (2,4)  (2,5)  (2,6)
+		(1,0)  (1,1)  (1,2)  (1,3)  (1,4)  (1,5)  (1,6)
+		(0,0)  (0,1)  (0,2)  (0,3)  (0,4)  (0,5)  (0,6)
+		where 0 = empty, 1 = player 1, 2 = player 2
+	"""
+	def __init__(self):
+		self.playerJustMoved = 2 # At the root pretend the player just moved is p2 - p1 has the first move
+		self.board = [] # 0 = empty, 1 = player 1, 2 = player 2
+		for i in range(6):
+			self.board.append([0]*7)
+		self.heights = [0]*7
 
+	def Clone(self):
+		""" Create a deep clone of this game state.
+		"""
+		st = Connect4State()
+		st.playerJustMoved = self.playerJustMoved
+		st.board = self.board
+		st.heights = self.heights
+		return st
+
+	def DoMove(self, move):
+		""" Update a state by carrying out the given move.
+			Must update playerJustMoved.
+		"""
+		self.playerJustMoved = 3 - self.playerJustMoved
+		assert self.heights[move] < 6
+		self.board[self.heights[move]][move] = self.playerJustMoved
+		self.heights[move] += 1
+		
+	def UndoMove(self, lastmove):
+		""" Update a state by undoing the given move.
+			Must update playerJustMoved.
+		"""
+		self.heights[lastmove] -= 1
+		self.board[self.heights[lastmove]][lastmove] = 0
+		self.playerJustMoved = 3 - self.playerJustMoved
+
+	def GetMoves(self):
+		""" Get all possible moves from this state.
+			in pseudocode:
+			First check if game is over.
+			if yes: no moves left
+			if no: return all columns that haven't been filled to the top
+		"""
+		winlocations  = () # update with all 69 possible win locations as a tuple of tuple of tuples
+		winchecks = []
+		for location in winlocations:
+			wc = []
+			for h, c in location:
+				wc.append(state.board[h][c])
+			winchecks.append(wc)
+		for w, x, y, z in winchecks:  
+			if w = x = y = z != 0:
+				return []
+		moves = [i for i in range(6) if self.heights[i] < 6]
+		return moves
+
+	def GetResult(self, playerjm):
+		""" Get the game result from the viewpoint of playerjm. 
+		"""
+		winlocations  = () # update with all 69 possible win locations as a tuple of tuple of tuples
+		winchecks = []
+		for location in winlocations:
+			wc = []
+			for h, c in location:
+				wc.append(state.board[h][c])
+			winchecks.append(wc)
+		for w, x, y, z in winchecks:  
+			if w = x = y = z != 0:
+				if self.playerJustMoved == playerjm:
+					return 1
+				else:
+					return 0
+		if self.GetMoves() == []: return 0.5 # draw
+		assert False # shouldn't be possible to get here
+				
+
+	def __repr__(self):
+		""" Don't need this - but good style.
+		"""
+		places = ((5,0)  (5,1)  (5,2)  (5,3)  (5,4)  (5,5)  (5,6)\
+				  (4,0)  (4,1)  (4,2)  (4,3)  (4,4)  (4,5)  (4,6)\
+				  (3,0)  (3,1)  (3,2)  (3,3)  (3,4)  (3,5)  (3,6)\
+				  (2,0)  (2,1)  (2,2)  (2,3)  (2,4)  (2,5)  (2,6)\
+				  (1,0)  (1,1)  (1,2)  (1,3)  (1,4)  (1,5)  (1,6)\
+				  (0,0)  (0,1)  (0,2)  (0,3)  (0,4)  (0,5)  (0,6))
+		s = " "
+		for h, c in places:
+			s += "_12"[self.board[h][c]] + " "
+			if c == 6: s += "\n "
+		return s
+			
 class OthelloState:
     """ A state of the game of Othello, i.e. the game board.
         The board is a 2D array where 0 = empty (.), 1 = player 1 (X), 2 = player 2 (O).
@@ -380,7 +480,8 @@ def UCTPlayGame():
         of UCT iterations (= simulations = tree nodes).
     """
     # state = OthelloState(4) # uncomment to play Othello on a square board of the given size
-    state = OXOState() # uncomment to play OXO
+    # state = OXOState() # uncomment to play OXO
+    state = Connect4State() # uncomment to play Connect 4
     # state = NimState(15) # uncomment to play Nim with the given number of starting chips
     while (state.GetMoves() != []):
         print(repr(state))
@@ -396,6 +497,22 @@ def UCTPlayGame():
         print("Player " + str(3 - state.playerJustMoved) + " wins!")
     else: print("Nobody wins!")
 
+def GetHumanMove():
+    """ Ask human player for move and determine if it's legal.
+		If not, ask again - if so, return that move
+    """
+    m = eval(input("These are the legal moves:\n" + str(state.GetMoves()) + "\nWhat's your move?\n"))
+    legal = 0
+	for legal_move in state.GetMoves():
+		if m == legal_move:
+			legal += 1
+    while legal == 0:
+		m = eval(input("Error: Invalid move.\nThese are the legal moves:\n" + str(state.GetMoves()) + "\n"))
+        for legal_move in state.GetMoves():
+            if m == legal_move:
+                legal += 1
+	return m
+        
 def UTCPlayVsHuman():
     """ Play a sample game between UCT player and human. Computer plays first.
         Number of UCT iterations is adjustable
@@ -403,9 +520,10 @@ def UTCPlayVsHuman():
     playagain = "y"
     while playagain == "y":
         # state = OthelloState(4) # uncomment to play Othello on a square board of the given size
-        state = OXOState() # uncomment to play OXO
+        # state = OXOState() # uncomment to play OXO
+        state = Connect4State() # uncomment to play Connect 4
         # state = NimState(15) # uncomment to play Nim with the given number of starting chips
-        iterations = eval(input("How many UTC iterations does the Computer opponent get?\n(The more iterations the better it plays, but any \nnumber nabove 10000 will cause it to run slowly)\n"))
+        iterations = eval(input("How many UTC iterations does the Computer opponent get?\n(The more iterations the better it plays, but any \nnumber above 10000 will cause it to run slowly)\n"))
         while isinstance(iterations, int) != True or iterations < 0:
             iterations = eval(input("Error: Please input a positive integer.\n"))
         humanplayer = eval(input("Would you like to be player 1 or 2?\n"))
@@ -416,9 +534,7 @@ def UTCPlayVsHuman():
             if state.playerJustMoved == humanplayer:
                 m = UCT(rootstate = state, itermax = iterations, verbose = False)
             else:
-                m = eval(input("Where would you like to place your piece? \n0 1 2\n3 4 5\n6 7 8\n")) # Change if not playing OXO
-                while state.board[m] != 0:
-                    m = eval(input("Error: Please pick an empty location.\nThese are the remaining empty locations:\n" + str(state.GetMoves()) + "\n"))
+                m = GetHumanMove()
             state.DoMove(m)
         if state.GetResult(state.playerJustMoved) == humanplayer:
             print(repr(state) + "\nYou win!!\n")
