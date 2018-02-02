b27d2ee92d6a5f4d4f55bc74c5ccf9e3ac707baf
thebmeister <35781415+thebmeister@users.noreply.github.com>
2018-02-02 15:12:36 -0800

    Update connect4engine_allcode.py
    added regularization using lmda/n = .0001

--- connect4engine_allcode.py
+++ connect4engine_allcode.py
@@ -63,7 +63,7 @@ class Network(object):
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
+        self.weights = [0.9999*w-(eta/len(mini_batch))*nw
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
